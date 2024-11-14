1869bf7e1775f5faa400ca2e607d6d684332453e
Animus1017 <slayergaming2k20@gmail.com>
2024-11-14 20:01:18 +0530

    4 commit

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -4,7 +4,7 @@ const Tours = ({tours,removeTour}) => {
   return (
     <div className='w-[1080px] py-10 mx-auto flex flex-col gap-10 items-center'>
       <h1 className='text-center text-4xl font-extrabold border-dashed border-8 border-indigo-400 px-16 py-4 w-fit rounded-xl'>Plan with Mahi</h1>
-      <div className='flex gap-2 justify-center items-center flex-wrap'>
+      <div className='flex gap-2 justify-center flex-wrap'>
         {
           tours.map((tour)=>{
             return <Card {...tour} removeTour={removeTour} className=''></Card>
